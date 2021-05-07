482d55ecee622f654c4b2667d3e4fa352b1e17cc
Juan <jgracila.everis@gmail.com>
2021-05-07 13:03:18 +0200

    Added linkedin to contact

--- src/global/keys.ts
+++ src/global/keys.ts
@@ -1,6 +1,7 @@
 const keys = {
     CONTACT_EMAIL_EMAIL: "jgrzz.menel@gmail.com",
     CONTACT_PHONE_NUMBER: "676 57 18 28",
+    CONTACT_LINKEDIN_PROFILE: "https://www.linkedin.com/in/juan-gracia-a029171a0"
 }
 
 export default keys;
