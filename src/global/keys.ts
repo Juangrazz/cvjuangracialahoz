abe9e4a8ee2c75b7bfdd0a7cee4dd09ffeb5417d
Juan Gracia <juangrazz@hotmail.com>
2021-06-20 20:26:25 +0200

    Fixes

--- src/global/keys.ts
+++ src/global/keys.ts
@@ -4,7 +4,9 @@ const keys = {
     CONTACT_LINKEDIN_PROFILE: "https://www.linkedin.com/in/juan-gracia-a029171a0",
     REPOSITORY_URL: "https://github.com/Juangrazz?tab=repositories",
     PEOPLE_FINDER_URL: "https://peoplefinder-34847.web.app/",
-    CV_REPOSITORY: "https://cvjuangracialahoz.netlify.app/"
+    CV_REPOSITORY: "https://cvjuangracialahoz.netlify.app/",
+    PEOPLE_FINDER_IMAGE_URL: "assets/img/people_finder.png",
+    CV_IMAGE_URL: "assets/img/cv_project.png",
 }
 
 export default keys;
