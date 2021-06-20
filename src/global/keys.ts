cff81bb70f65490e87f1bfee52e22315a846e95d
Juan Gracia <juangrazz@hotmail.com>
2021-06-20 19:38:20 +0200

    Updated project section

--- src/global/keys.ts
+++ src/global/keys.ts
@@ -1,7 +1,10 @@
 const keys = {
     CONTACT_EMAIL_EMAIL: "jgrzz.menel@gmail.com",
     CONTACT_PHONE_NUMBER: "676 57 18 28",
-    CONTACT_LINKEDIN_PROFILE: "https://www.linkedin.com/in/juan-gracia-a029171a0"
+    CONTACT_LINKEDIN_PROFILE: "https://www.linkedin.com/in/juan-gracia-a029171a0",
+    REPOSITORY_URL: "https://github.com/Juangrazz?tab=repositories",
+    PEOPLE_FINDER_URL: "https://peoplefinder-34847.web.app/",
+    CV_REPOSITORY: "https://cvjuangracialahoz.netlify.app/"
 }
 
 export default keys;
